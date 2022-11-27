75c8590cf1d537ab6b7f141f978a26b5cd075a08
VictoryBlue <88573273@qq.com>
2022-11-27 22:44:40 +0800

    lecture 16

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './Navbar';
 import Navbar from './Navbar';
 import Home from './Home'
+
 function App() {
-  const title = "hello world";
-  const likes = 50;
-  // const person = {title:'??'};
-  const link = "http://www.google.com";
   return (
     <div className="App">
       <Navbar/>
--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,39 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 const Home = () => {
+  const [name, setName] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => res.json())
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+  });
+
   const [blogs, setBlogs] = useState([
-   { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);// [initialValue ChangeValueFun]
- 
- 
- //  const handleClickAgain = (name, e) =>{
- //   console.log('hello ' + name , e.target)
- //  }
- return ( 
-   <div className="home">
-     
-     {blogs.map((blog)=>(
-       <div className="blog-preview" key={blog.id}>
-         <h2 >{blog.title}</h2>
-         <p>written by {blog.author}</p>
-         <p>{blog.body}</p>
-       </div>
-     ))}
-     {/* Note:将事件作为参数传入 */}
-     {/* <button onClick = {(e)=>{
-       handleClickAgain('willow',e)
-     }}>Click me again</button> */}
-   </div>
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ]); // [initialValue ChangeValueFun]
+
+  return (
+    <div className="home">
+      {isPending && <div>Loading</div>}
+      {blogs && <BlogList blogs={blogs} title={'All Blogs'}></BlogList>}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
+    </div>
   );
-}
+};
 
 export default Home;
