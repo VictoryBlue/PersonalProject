60b5b16754256eca9a21bb5c4305b80afa97e598
VictoryBlue <88573273@qq.com>
2022-11-28 11:50:17 +0800

    完善请求数据接口

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,36 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+
 const Home = () => {
   const [name, setName] = useState(null);
   const [isPending, setIsPending] = useState(true);
-
+  const [error, setError] = useState(null);
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
-        .then((res) => res.json())
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
         .then((data) => {
           setBlogs(data);
+          setIsPending(false);
+          setError(null)
+        })
+        .catch((err) => {
+          setError(err.message);
           setIsPending(false);
         });
     }, 1000);
   });
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]); // [initialValue ChangeValueFun]
+  const [blogs, setBlogs] = useState(null); // [initialValue ChangeValueFun]
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
       {isPending && <div>Loading</div>}
       {blogs && <BlogList blogs={blogs} title={'All Blogs'}></BlogList>}
       <button onClick={() => setName('luigi')}>change name</button>
