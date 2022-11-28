8ec482a3809a93decde42d425c1084c76ef244a8
VictoryBlue <88573273@qq.com>
2022-11-28 12:09:55 +0800

    重构请求借口代码

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,19 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [name, setName] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('could not fetch the data for that resource');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null)
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  });
-
-  const [blogs, setBlogs] = useState(null); // [initialValue ChangeValueFun]
-
+  // const [blogs, setBlogs] = useState(null); // [initialValue ChangeValueFun]
+  let {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch('http://localhost:8000/blogs');
   return (
     <div className="home">
       {error && <div> {error} </div>}
       {isPending && <div>Loading</div>}
       {blogs && <BlogList blogs={blogs} title={'All Blogs'}></BlogList>}
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{name}</p>
     </div>
   );
 };
