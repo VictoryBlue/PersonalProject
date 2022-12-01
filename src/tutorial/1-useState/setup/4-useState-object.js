7e26203fb50b58eac280a827ac708ee251f99049
VictoryBlue <88573273@qq.com>
2022-12-02 00:47:15 +0800

    useState with object2

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,15 +6,21 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   });
+  const [name, setName] = useState('peter');
+  const [Age, setAge] = useState(24);
+  const [Message, setMessage] = useState('random message');
   // Note:仅仅改变message的值，需要将person传入后重新覆盖旧的message
-  const chagneMessage = () => {
-    setPerson({ ...person, message: 'Hello world' });
-  };
+  // const chagneMessage = () => {
+  //   setPerson({ ...person, message: 'Hello world' });
+  // };
+  const chagneMessage = ()=>{
+    setMessage('Hello world');
+  }
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{Age}</h3>
+      <h3>{Message}</h3>
       <button className="btn" onClick={chagneMessage}>
         change message
       </button>
