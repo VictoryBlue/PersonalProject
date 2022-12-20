3047d9b20261f552e599463d42e550c134554137
VictoryBlue <88573273@qq.com>
2022-12-20 17:30:09 +0800

    prop-types可以对组件的props进行校验

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,26 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import defaultImage from '../../../assets/default-image.jpeg';
+const Product = ({ image, name, price }) => {
+  return (
+    <article className="product">
+      <h4>single product</h4>
+      <img src={image.url || defaultImage} alt="name" />
+      <h4>{name}</h4>
+      <p>${price}</p>
+    </article>
+  );
 };
-
+// 可以引入prop-types包对组件的props进行校验
+// 组件.propTypes = {}
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+Product.defaultProps = {
+  name: 'default name',
+  price: 3.99,
+  image: defaultImage,
+};
 export default Product;
