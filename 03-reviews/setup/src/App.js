0a666471357144ab87589470bf718846fd370464
VictoryBlue <88573273@qq.com>
2022-12-21 23:04:08 +0800

    nothing special 哪里需要使用数据在哪里import数据就好

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
 import Review from './Review';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <div className="title">
+          <h2>our Reviews</h2>
+          <div className="underline"></div>
+          <Review />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,48 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  console.log(people);
+  const handleRight = () => {
+    setIndex((index + 1) % people.length);
+  };
+  const handleLeft = () => {
+    setIndex(index - 1 < 0 ? people.length - 1 : index - 1);
+  };
+  const random = () => {
+    return setIndex(parseInt(Math.random(0, 1) * people.length));
+  };
+  const { name, job, text, image } = people[index];
+
+  return (
+    <>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+
+        <p>{name}</p>
+        <p className="job">{job}</p>
+        <p>{text}</p>
+        <div>
+          <FaChevronLeft
+            className="prev-btn"
+            onClick={handleLeft}
+          ></FaChevronLeft>
+          <FaChevronRight
+            className="next-btn"
+            onClick={handleRight}
+          ></FaChevronRight>
+        </div>
+        <button className="random-btn" onClick={random}>
+          Surprise Me
+        </button>
+      </article>
+    </>
+  );
 };
 
 export default Review;
