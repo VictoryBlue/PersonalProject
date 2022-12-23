4129788aabac773e88311ad0cd3526f370967b0b
VictoryBlue <88573273@qq.com>
2022-12-23 15:57:58 +0800

    menu

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
 import items from './data';
-
+import Categories from './Categories';
+import Menu from './Menu';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => {
+      return item.category === category;
+    });
+    setMenuItems(newItems);
+  };
   return (
     <main>
-      <div className="container">
-        <h3>questions and answers about login</h3>
-        <section className="info">
-          {questions.map((question) => {
-            return <SingleQuestion key={question.id} {...question} />;
-          })}
-        </section>
-      </div>
+      <section className="container">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+          <Categories categories={categories} filterItems={filterItems} />
+          <Menu items={menuItems} className="menu" />
+        </div>
+      </section>
     </main>
   );
 }
