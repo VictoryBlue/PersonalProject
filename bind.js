58f5a6653035ae5633035ce0240d0418d4e2e979
VictoryBlue <88573273@qq.com>
2023-01-17 17:05:36 +0800

    day3

--- bind.js
+++ bind.js
@@ -1,3 +1,4 @@
+// bind就是说，你要用一个函数func，但是这个函数的this的指向不是你想要的，bind返回一个函数，这个函数和func的执行逻辑相同【传参，执行，返回值】但是这个函数的this是已经指定好的 bind(context)中的context。
 // bind的具体用法
 const module = {
   x: 42,
@@ -5,7 +6,15 @@ const module = {
     return this.x;
   },
 };
-
+Function.prototype.myBind = function (context, ...args1) {
+  const self = this;
+  return function F(...args2) {
+    if (self instanceof F) {
+      return new F()
+    }
+    return self.apply(context, [...args1, ...args2]);
+  };
+};
 const unboundGetX = module.getX;
 console.log(unboundGetX()); // unboundGetX 声明为全局函数
 // expected output: undefined
