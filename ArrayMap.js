784ab024ceccc4af6116bab56e74e2db7de83e88
VictoryBlue <88573273@qq.com>
2023-02-13 16:49:34 +0800

    day9

---
+++ ArrayMap.js
@@ -0,0 +1,21 @@
+Array.prototype._map = function (f) {
+  ans = [];
+  for (let i in this) {
+    ans.push(f(this[i]));
+  }
+  return ans;
+};
+a = [1, 2, 3]._map((i) => i * 2);
+console.log(a); //[ 2, 4, 6, NaN ]
+// 错误原因，for in 不止会遍历数组的索引，还遍历了添加的方法
+// 所以应该写
+
+Array.prototype._map = function (f) {
+  ans = [];
+  for (i = 0; i < this.length; i++) {
+    ans.push(f(this[i]));
+  }
+  return ans;
+};
+a = [1, 2, 3]._map((i) => i * 2);
+console.log(a); //[ 2, 4, 6 ]
