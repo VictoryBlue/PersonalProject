10ce03ca194457a834bdd346466c3220a5f0a879
VictoryBlue <88573273@qq.com>
2023-04-30 23:12:10 +0800

    day9

--- ObjectCreate.js
+++ ObjectCreate.js
@@ -1,6 +1,9 @@
 // Object.create 函数是实现继承的一种方法
 // 调用new Child()会自动把child实例上的__proto__指向Child.prototype.
 // 因此继承的时候应该写 Child.prototype = Object.create(Parent.prototype)
+// 为什么用Object.create实现继承呢？
+// 是因为原型链的缘故，当在当前对象找不到的方法就顺着__proto__到__proto__所指的对象上去找。
+// 由此的child.prototype = Object.create(parent.prototype)
 function create(proto) {
   function F() {}
   F.prototype = proto;
