91fd5b827cbc25604df71408d5f23a8253c38fa5
VictoryBlue <88573273@qq.com>
2023-09-01 16:11:25 +0800

    docs

--- useLocalStorage.js
+++ useLocalStorage.js
@@ -1,6 +1,6 @@
 // 要的效果
 // 可以得到LocalStorage的值，可以改变LocalStorage的值
-
+// 用户偏好设置：主题颜色、语言选择
 function useLocalStorage(key, initialValue){
   const [value, setValue] = useState(initialValue||localStorage.getItem(key))
   useEffect(() => {
