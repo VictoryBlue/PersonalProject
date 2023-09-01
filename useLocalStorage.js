aa3d75d20c496670ffe706b856a6a619a9327486
VictoryBlue <88573273@qq.com>
2023-09-01 15:58:27 +0800

    bugfix

--- useLocalStorage.js
+++ useLocalStorage.js
@@ -2,7 +2,7 @@
 // 可以得到LocalStorage的值，可以改变LocalStorage的值
 
 function useLocalStorage(key, initialValue){
-  const [state, setValue] = useState(initialValue||localStorage.getItem(key))
+  const [value, setValue] = useState(initialValue||localStorage.getItem(key))
   useEffect(() => {
     localStorage.setItem(key, value)
   }, [value, key])
